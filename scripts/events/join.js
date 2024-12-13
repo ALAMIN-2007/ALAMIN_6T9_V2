f53260d4dac35ab3a5896b1651164cae8076afa8
ALAMIN-2007 <140133353+ALAMIN-2007@users.noreply.github.com>
2024-12-13 10:48:03 +0600

    Update join.js

--- scripts/events/join.js
+++ scripts/events/join.js
@@ -48,7 +48,7 @@ module.exports.run = async function({ api, event, Users }) {
   if (event.logMessageData.addedParticipants && Array.isArray(event.logMessageData.addedParticipants) && event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
     //api.changeNickname(`𝗕𝗢𝗧 ${(!global.config.BOTNAME) ? "Buddy" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
 
-    let gifUrl = 'https://i.imgur.com/wIXgQgt.jpeg';
+    let gifUrl = 'https://i.postimg.cc/brsWyT9f/join.gif';
 let gifPath = __dirname + '/Nayan/join/join.gif';
 
 axios.get(gifUrl, { responseType: 'arraybuffer' })
@@ -184,4 +184,4 @@ https://www.facebook.com/MDALAMINHOSENNOBITA
       }
     } catch (e) { return console.log(e) };
   }
-  }
+}
